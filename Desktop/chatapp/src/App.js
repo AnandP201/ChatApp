5f13d3f1b3ba7a955a680ab5250166e6f1218259
AnandP201 <pie1002anand@gmail.com>
2021-06-30 09:58:18 +0530

    created public and private routes

--- Desktop/chatapp/src/App.js
+++ Desktop/chatapp/src/App.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import 'rsuite/dist/styles/rsuite-default.css';
 import './styles/main.scss';
+import { Switch } from 'react-router-dom';
+import Signin from './pages/Signin';
+import Home from './pages/Home';
+import PrivateRoute from './Components/PrivateRoute';
+import PublicRoute from './Components/PublicRoute';
 
 function App() {
-  return <div>Hellos</div>;
+  return (
+    <Switch>
+      <PublicRoute path="/signin">
+        <Signin />
+      </PublicRoute>
+      <PrivateRoute path="/">
+        <Home />
+      </PrivateRoute>
+    </Switch>
+  );
 }
 
 export default App;
---
+++ Desktop/chatapp/src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const config = {
+  apiKey: 'AIzaSyDTcZ4Gc-2_X1Rhh-hSNINFmR7KnZYCk-o',
+  authDomain: 'chatapp-62d6c.firebaseapp.com',
+  projectId: 'chatapp-62d6c',
+  storageBucket: 'chatapp-62d6c.appspot.com',
+  messagingSenderId: '605286216295',
+  appId: '1:605286216295:web:761a38d5b380fedd055ac2',
+};
+
+const app = firebase.initializeApp(config);
