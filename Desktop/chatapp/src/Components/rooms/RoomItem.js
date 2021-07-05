8bd67ed88de80387cbd0b9716701585ca0f5689b
AnandP201 <pie1002anand@gmail.com>
2021-07-05 13:47:52 +0530

    displayed chat rooms

--- Desktop/chatapp/src/Components/rooms/RoomItem.js
+++ Desktop/chatapp/src/Components/rooms/RoomItem.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import TimeAgo from 'timeago-react';
 
-const RoomItem = () => (
-  <div>
-    <div className="d-flex justify-content-between align-items-center">
-      <h3 className="text-disappear">Room name</h3>
-      <TimeAgo datetime={new Date()} className="font-normal text-black-45" />
-    </div>
+const RoomItem = ({ room }) => {
+  const { createdAt, name } = room;
 
-    <div className="d-flex align-items-center text-black-70">
-      <span>No messages yet...</span>
+  return (
+    <div>
+      <div className="d-flex justify-content-between align-items-center">
+        <h3 className="text-disappear">{name}</h3>
+
+        <TimeAgo
+          datetime={new Date(createdAt)}
+          className="font-normal text-black-45"
+        />
+      </div>
+
+      <div className="d-flex align-items-center text-black-70">
+        <span>No messages yet...</span>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 export default RoomItem;
