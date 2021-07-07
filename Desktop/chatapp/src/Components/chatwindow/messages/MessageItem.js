2eb91d165a43e944939b9bb217d46679f12ee402
AnandP201 <pie1002anand@gmail.com>
2021-07-07 10:05:03 +0530

    added functionality to like message

--- Desktop/chatapp/src/Components/chatwindow/messages/MessageItem.js
+++ Desktop/chatapp/src/Components/chatwindow/messages/MessageItem.js
@@ -2,17 +2,18 @@ import React, { memo } from 'react';
 import { Button } from 'rsuite';
 import TimeAgo from 'timeago-react';
 import { useCurrentRoom } from '../../../context/currentRoom.context';
-import { useHover } from '../../../misc/custom.hooks';
+import { useHover, useMediaQuery } from '../../../misc/custom.hooks';
 import { auth } from '../../../misc/firebase';
 import PresenceDot from '../../PresenceDot';
 import ProfileAvatar from '../../ProfileAvatar';
 import IconBtnControl from './IconBtnControl';
 import ProfileInfoModel from './ProfileInfoModel';
 
-const MessageItem = ({ message, handleAdmin }) => {
-  const { author, createdAt, text } = message;
+const MessageItem = ({ message, handleAdmin, handleLike }) => {
+  const { author, createdAt, text, likes, likeCount } = message;
 
   const [selfRef, isHovered] = useHover();
+  const isMobile = useMediaQuery('(max-width:992px)');
 
   const isAdmin = useCurrentRoom((v) => v.isAdmin);
   const admins = useCurrentRoom((v) => v.admins);
@@ -21,6 +22,9 @@ const MessageItem = ({ message, handleAdmin }) => {
   const isAuthor = auth.currentUser.uid === author.uid;
 
   const canGrantAdmin = isAdmin && !isAuthor;
+
+  const canShowIcons = isMobile || isHovered;
+  const isLiked = likes && Object.keys(likes).includes(auth.currentUser.uid);
 
   return (
     <li
@@ -54,12 +58,12 @@ const MessageItem = ({ message, handleAdmin }) => {
         />
 
         <IconBtnControl
-          {...(true ? { color: 'red' } : {})}
-          isVisible
+          {...(isLiked ? { color: 'red' } : {})}
+          isVisible={canShowIcons}
           iconName="heart"
-          tooltip="Like this message"
-          onClick={() => {}}
-          bagdeContent={5}
+          tooltip={likeCount > 0 ? 'Unlike this message' : 'Like this message'}
+          onClick={() => handleLike(message.id)}
+          badgeContent={likeCount}
         />
       </div>
       <div>
